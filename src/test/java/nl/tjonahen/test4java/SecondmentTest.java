271a29ead17eadf9e79824fb3aeabc3a4493e4d0
Philippe Tjon-A-Hen <philippe@tjonahen.nl>
2018-01-12 12:31:41 +0100

    Renamed boundry ad removed beans.xml

--- src/test/java/nl/tjonahen/test4java/SecondmentTest.java
+++ src/test/java/nl/tjonahen/test4java/SecondmentTest.java
@@ -30,6 +30,7 @@ import java.io.File;
 import javax.inject.Inject;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import static org.junit.Assert.assertEquals;
 import org.junit.runner.RunWith;
@@ -43,7 +44,6 @@ import org.junit.runner.RunWith;
 public class SecondmentTest {
 
     private static final String A_COMPANY = "ACompany";
-    private static final String WEBAPP_SRC = "src/main/webapp";
 
     private String devName;
     private final WireMock wiremock = new WireMock(8888);    
@@ -51,33 +51,35 @@ public class SecondmentTest {
     @Deployment
     public static JavaArchive createDeployment() {
         return ShrinkWrap.create(JavaArchive.class)
-                .addPackage(SecondmentBoundry.class.getPackage())
-                .addAsManifestResource(new File(WEBAPP_SRC, "WEB-INF/beans.xml"));
+                .addPackage(SecondmentEndPoint.class.getPackage())
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
     @Inject
-    private SecondmentBoundry boundry;
+    private SecondmentEndPoint endpoint;
 
     @Given("^A company with a java developer \"([^\"]*)\"$")
     public void a_company_with_a_java_developer(String devName) throws Throwable {
-        boundry.addCompany(A_COMPANY);
-        boundry.addDeveloper(A_COMPANY, devName);
+        endpoint.addCompany(A_COMPANY);
+        endpoint.addDeveloper(A_COMPANY, devName);
         this.devName = devName;
     }
 
     @When("^We send the developer on a job at \"([^\"]*)\"$")
     public void we_send_the_developer_on_a_job_at(String jobName) throws Throwable {
-            wiremock.register(post(urlEqualTo("/"+jobName))
+        wiremock.register(post(urlEqualTo("/"+jobName))
                     .withRequestBody(containing(devName))
                     .willReturn(aResponse()
                             .withStatus(200)
                             .withBody("0")));
 
-        boundry.sendDeveloperOnJob(A_COMPANY, this.devName, jobName);
+        endpoint.sendDeveloperOnJob(A_COMPANY, this.devName, jobName);
+        
+        wiremock.verifyThat(WireMock.postRequestedFor(urlEqualTo("/"+jobName)));
     }
 
     @Then("^We recieve money$")
     public void we_recieve_money() throws Throwable {
-        assertEquals("0", boundry.getEarnings(A_COMPANY));
+        assertEquals("0", endpoint.getEarnings(A_COMPANY));
     }
 }
